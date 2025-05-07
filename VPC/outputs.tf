04509e5f4f261e8040863529f71bf6e5b7947696
MB.Kalpana <70999645+MBkalpana@users.noreply.github.com>
2025-05-07 10:54:22 +0530

    Update and rename output.tf to outputs.tf

---
+++ VPC/outputs.tf
@@ -0,0 +1,27 @@
+output "vpc_id"{
+  description = "vpc_id"
+  value = aws_vpc.main.id
+}
+output "private_subnet_ids"{
+  description = "private_subnet_ids"
+  value = aws_subnet.private[*].id
+}
+output "public_subnet_ids"{
+  description = "public_subnet_ids"
+  value = aws_subnet.public[*].id
+}
+
+output "source_bucket"{
+  description = "source_bucket"
+  value = aws_s3_bucket.source_bucket.bucket
+}
+
+output "processed_bucket "{
+  description = "processed_bucket"
+  value = aws_s3_bucket.processed_bucket.bucket
+}
+
+output "lambda_function_name"{
+  description = "processed_bucket"
+  value = aws_s3_bucket.processed_bucket.bucket
+}
